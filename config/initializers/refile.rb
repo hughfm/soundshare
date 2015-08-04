98721082b829d32d77b30baae2bc18b1d3672010
Hugh Middleton <hughfmiddleton@gmail.com>
2015-08-04 19:04:20 +1000

    configure env variables

---
+++ config/initializers/refile.rb
@@ -0,0 +1,12 @@
+require "refile/s3"
+
+aws = {
+  access_key_id: ENV['S3_KEY'],
+  secret_access_key: ENV['S3_SECRET'],
+  region: "ap-southeast-2",
+  bucket: "hughfm-wdi-project-2",
+}
+
+Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+Refile.store = Refile::S3.new(prefix: "store", **aws)
+Refile.host = "//d380ny7ob5f9xn.cloudfront.net"
